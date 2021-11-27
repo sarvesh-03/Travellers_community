0858851e17caae5f220a0e1e832e4a5e81a5c2d7
sarvesh-03 <sarveshjan18@gmail.com>
2021-11-27 17:24:23 +0530

    feat:implement mvvm

--- app/src/main/java/com/example/travellerscommunity/ui/activity/DashboardActivity.kt
+++ app/src/main/java/com/example/travellerscommunity/ui/activity/DashboardActivity.kt
@@ -1,7 +1,9 @@
-package com.example.travellerscommunity
+package com.example.travellerscommunity.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.travellerscommunity.R
+
 //This activity is host for remaining fragments
 class DashboardActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/travellerscommunity/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/travellerscommunity/ui/activity/MainActivity.kt
@@ -1,7 +1,9 @@
-package com.example.travellerscommunity
+package com.example.travellerscommunity.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.travellerscommunity.R
+
 //Login and registration can be done here
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
